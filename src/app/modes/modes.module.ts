6e7241ab8cba96f3ddd4cbee8b1d78236d592cee
Ericcentrik <enaiman1@gmail.com>
2020-03-17 12:45:43 -0400

    created/added the 4 modules to create the sperate pages

---
+++ src/app/modes/modes.module.ts
@@ -0,0 +1,14 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { ModesRoutingModule } from './modes-routing.module';
+
+
+@NgModule({
+  declarations: [],
+  imports: [
+    CommonModule,
+    ModesRoutingModule
+  ]
+})
+export class ModesModule { }
